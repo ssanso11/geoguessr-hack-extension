f44f27fb8d0165b55284dbba3db9b05707e44f57
acavet <acavet@gmail.com>
2021-05-18 21:55:25 -0700

    temporary refresh fix: delay by 0.8 seconds to allow new data to load after refresh

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 // here
 function open_map(lat, lng) {
     const url = "https://google.com/maps/place/" + lat + ", " + lng; // eventually pass in lat lng
-    window.open(url);
+    //window.open(url);
     console.log(lat);
     console.log(lng);
 }
@@ -10,19 +10,27 @@ function open_map(lat, lng) {
 // open in maps button
 openMapsBtn.addEventListener("click", function () {
     chrome.tabs.query({ active: true }, function (tabs) {
-        const { id: tabId } = tabs[0].url;
-        chrome.tabs.executeScript(tabId, { runAt: 'document_start', code: 'document.querySelectorAll("#__NEXT_DATA__")[0].text' }, function (result) {
-            const json_next_data = JSON.parse(result[0]);
-            const rounds = json_next_data.props.pageProps.game.rounds;
-            console.log(rounds);
-            const thisRound = rounds[rounds.length - 1];
-            const { lat, lng } = thisRound;
-            open_map(lat, lng);
-        });
-    });
-});
-refreshPageBtn.addEventListener("click", function () {
-    chrome.tabs.query({ active: true }, function (tabs) {
         chrome.tabs.reload();
+        console.log("waiting...")
     });
+    setTimeout(() => {
+        chrome.tabs.query({ active: true }, function (tabs) {
+            const { id: tabId } = tabs[0].url;
+            chrome.tabs.executeScript(tabId, { runAt: 'document_end', code: 'document.querySelectorAll("#__NEXT_DATA__")[0].text' }, function (result) {
+                const json_next_data = JSON.parse(result[0]);
+                const rounds = json_next_data.props.pageProps.game.rounds;
+                console.log(rounds);
+                const thisRound = rounds[rounds.length - 1];
+                const { lat, lng } = thisRound;
+                open_map(lat, lng);
+            });
+        });
+    }, 800);
 });
+
+// refreshPageBtn.addEventListener("click", function () {
+//     chrome.tabs.query({ active: true }, function (tabs) {
+//         chrome.tabs.reload();
+
+//     });
+// });
