af17ff9309e964cd5ca9a57fe56b3ee0984d3633
acavet <acavet@gmail.com>
2021-05-20 10:02:54 -0700

    added distance from location option

--- popup.js
+++ popup.js
@@ -1,13 +1,18 @@
+const earthRadius = 6975153.941; // approx earth radius in yards
+
 // submit location in geoguessr
-function guess_location(lat, lng, token) {
-    console.log(lat, lng);
+function guess_location(lat, lng, yardsAway, token) {
+    console.log(lat, lng, yardsAway);
+    theta = Math.random() * 2 * Math.PI
+    newLat = lat + (yardsAway * Math.sin(theta) / earthRadius) * (180 / Math.PI);
+    newLng = lng + (yardsAway * Math.cos(theta) / earthRadius) * (180 / Math.PI) / Math.cos(lat * Math.PI / 180);;
     const xhr = new XMLHttpRequest();
     // post url is going to be the current game url
     const post_url = "https://www.geoguessr.com/api/v3/games/" + token;
     xhr.open("POST", post_url)
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     // send lat, lng, and game url token in request
-    xhr.send(JSON.stringify({ "lng": lng, "lat": lat, "timedOut": false, "token": token }));
+    xhr.send(JSON.stringify({ "lng": newLng, "lat": newLat, "timedOut": false, "token": token }));
 }
 
 // open location in google maps
@@ -35,11 +40,12 @@ submitBtn.addEventListener("click", function () {
             chrome.tabs.executeScript(tabId, { runAt: 'document_end', code: 'document.querySelectorAll("#__NEXT_DATA__")[0].text' }, function (result) {
                 const json_next_data = JSON.parse(result[0]);
                 const rounds = json_next_data.props.pageProps.game.rounds;
-                // console.log(json_next_data.props.pageProps.game)
                 const { lat, lng } = (rounds[rounds.length - 1]);
                 const token = json_next_data.props.pageProps.game.token;
+                const yardsAway = document.getElementById('distanceInputYrds').value;
                 console.log(token);
-                guess_location(lat, lng, token);
+                guess_location(lat, lng, yardsAway, token);
+                reloadTab();
             });
         });
     }, 800);
